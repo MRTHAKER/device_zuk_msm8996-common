2398b17d9295db0ade18d7894df540a44f72f3b7
mrthaker <mahek.haker@gmail.com>
2018-01-22 16:21:02 +0000

    Revert "marlin/sailfish thermal HAL: move to thermal-engine"
    This reverts commit 0ddc49a7ffdbc660ded1aea682612df4787360b5.

--- msm8996.mk
+++ msm8996.mk
@@ -169,10 +169,12 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:system/etc/sec_config
 
 # Thermal
+PRODUCT_PACKAGES += \
+    thermal.msm8996
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf \
     $(LOCAL_PATH)/configs/thermal-engine-vr.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-vr.conf
-
 # IPv6
 PRODUCT_PACKAGES += \
     ebtables \
